e5fb70359b89800fd5a38afae3fded269c07b677
mosespace <kisakyemoses50@gmail.com>
2023-06-30 00:10:00 +0300

    feat! added map ion the array ✅

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 export default function ServiceCard({ data }) {
-  const { title, decription,  itemsArray } = data;
+  const { image, title, decription,  itemsArray } = data;
   return (
-    <main>
-      
-      <div className="content">
-      <h1>{title}</h1>
-      <p className='description'>{decription}</p>
-      {itemsArray.map((item, id)=>(
-        <p>{item}</p>
-      ))}
+    <section>
+      <div className="used-image">
+        <img src={image} />
       </div>
-    </main>
+        <div className="content">
+        <h1>{title}</h1>
+        
+        <p className='description'>{decription}</p>
+        {itemsArray.map((item, id) => (
+            <p key={id}>{item}</p>
+        ))}
+      </div></section>
   );
 }
 console.log(ServiceCard);
