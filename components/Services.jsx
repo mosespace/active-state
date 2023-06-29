b0f4e0b7aed293e1dec4e57221b5b2c034263307
mosespace <kisakyemoses50@gmail.com>
2023-06-29 15:58:18 +0300

    fix! mapping in the Services

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 export default function ServiceCard({ data }) {
-  const { title, decription,  items } = data;
+  const { title, decription,  itemsArray } = data;
   return (
     <main>
+      
       <div className="content">
       <h1>{title}</h1>
       <p className='description'>{decription}</p>
-      <p>{items}</p>
+      {itemsArray.map((item, id)=>(
+        <p>{item}</p>
+      ))}
       </div>
     </main>
   );
