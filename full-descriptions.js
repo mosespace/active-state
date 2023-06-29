4e7ad1cea8c0f48dcfb493efee24b4fc75cece62
mosespace <kisakyemoses50@gmail.com>
2023-06-29 15:57:24 +0300

    feat! changed aray name

--- full-descriptions.js
+++ full-descriptions.js
@@ -6,17 +6,17 @@ export const AppDevelopment ={
     image: app,
     title: 'App development',
     decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero fugiat magnam sit veniam nihil similique aut eos quae quia!',
-    items: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
+    itemsArray: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
 };
 export const WebDevelopment ={
     image: web,
     title: 'Web development',
     decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero fugiat magnam sit veniam nihil similique aut eos quae quia!',
-    items: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
+    itemsArray: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
 };
 export const MachineLearning ={
     image: machine,
     title: 'Machine Learning',
     decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero fugiat magnam sit veniam nihil similique aut eos quae quia!',
-    items: ['Building Machine Applications', 'Hosting & Marketting Machine Learning the Web', 'Maintaince for Free Monthly!']
+    itemsArray: ['Building Machine Applications', 'Hosting & Marketting Machine Learning the Web', 'Maintaince for Free Monthly!']
 };
