97ace4f08c36857ba16d7168ab11e9300114c04e
mosespace <kisakyemoses50@gmail.com>
2023-06-29 19:43:19 +0300

    fix! renamed the items array ✅

--- full-descriptions.js
+++ full-descriptions.js
@@ -3,20 +3,23 @@ import machine from "./public/images-icons/machine.png";
 import web from "./public/images-icons/web.png";
 
 export const AppDevelopment ={
+    id: 1,
     image: app,
     title: 'App development',
     decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero fugiat magnam sit veniam nihil similique aut eos quae quia!',
-    items: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
+    itemsArray: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
 };
 export const WebDevelopment ={
+    id: 2,
     image: web,
     title: 'Web development',
     decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero fugiat magnam sit veniam nihil similique aut eos quae quia!',
-    items: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
+    itemsArray: ['Building Web Applications', 'Hosting the Web Applicationson the server', 'Maintaince for Free Monthly!']
 };
 export const MachineLearning ={
+    id: 3,
     image: machine,
     title: 'Machine Learning',
     decription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero fugiat magnam sit veniam nihil similique aut eos quae quia!',
-    items: ['Building Machine Applications', 'Hosting & Marketting Machine Learning the Web', 'Maintaince for Free Monthly!']
+    itemsArray: ['Building Machine Applications', 'Hosting & Marketting Machine Learning the Web', 'Maintaince for Free Monthly!']
 };
