46b7839740ee0d9397b2cf8e5541d5b2916e2d2d
mosespace <kisakyemoses50@gmail.com>
2023-06-29 19:41:59 +0300

    faet! added item mapping ✅

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 export default function ServiceCard({ data }) {
-  const { title, decription,  items } = data;
+  const { title, decription,  itemsArray } = data;
   return (
-    <main>
       <div className="content">
       <h1>{title}</h1>
       <p className='description'>{decription}</p>
-      <p>{items}</p>
+      {itemsArray.map((item, id) => (
+          <p key={id}>{item}</p>
+      ))}
       </div>
-    </main>
   );
-}
-console.log(ServiceCard);
+};
