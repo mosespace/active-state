da6d34c1da301a952160bc8318b94b7313d88f7c
mosespace <kisakyemoses50@gmail.com>
2023-06-30 18:29:37 +0300

    fix! styling and typo

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
 export default function ServiceCard({ data }) {
   const { image, title, decription,  itemsArray } = data;
   return (
     <section>
       <div className="used-image">
-        <img src={image} />
-      </div>
+        <Image src={image} width={100} height={100} alt={title}/>
+      
+
         <div className="content">
         <h1>{title}</h1>
         
@@ -14,6 +16,8 @@ export default function ServiceCard({ data }) {
         {itemsArray.map((item, id) => (
             <p key={id}>{item}</p>
         ))}
-      </div></section>
+      </div>
+      </div>
+    </section>
   );
 };
