efac56098839a925b3ed7ffb301f51b1a3d4436b
mosespace <kisakyemoses50@gmail.com>
2023-07-01 09:22:16 +0300

    feat! detailed page component ✅

--- app/libs/fetchproducts.js
+++ app/libs/fetchproducts.js
@@ -1,4 +1,4 @@
-export default async function getproducts() {
+export default async function fetchproducts() {
   const response = await fetch('https://fakestoreapi.com/products?limit=12')
     return await response.json()
 }
