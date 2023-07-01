1a9d77ead8474813f34114901a9d640d85dde883
mosespace <kisakyemoses50@gmail.com>
2023-07-01 09:17:03 +0300

    feat! removed import react tag

--- app/libs/getproducts.js
+++ app/libs/getproducts.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default async function getproducts() {
   const response = await fetch('https://fakestoreapi.com/products?limit=12')
     return await response.json()
